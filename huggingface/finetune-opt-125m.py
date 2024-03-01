58a37f770a0d9af56b2145e9e1dfc0f526c16ef7
MikkelGodsk <mikkelgodskj@hotmail.com>
2024-03-01 11:37:32 +0100

    Can overfit to single batch, and text handling is done correctly.

--- huggingface/finetune-opt-125m.py
+++ huggingface/finetune-opt-125m.py
@@ -2,16 +2,18 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import TrainingArguments
 from transformers.integrations import WandbCallback
 from trl import SFTTrainer
-from datasets import load_dataset
+from trl.trainer import DataCollatorForCompletionOnlyLM
+from datasets import load_dataset, Dataset
 import torch
 import wandb
 import numpy as np
 import os
 
 """
-TODO:
-- Overfit to single example
-- Validation set
+TODO:  Reproduce LIMA experiment &
+- Get WandB to log on work3!!   WORKS
+- Overfit to single batch       WORKS
+- Validation set                WORKS
 - Accelerate for multi gpu??
 - Gradient trick??
 - DPO?
@@ -19,9 +21,11 @@ TODO:
 - PEFT
 """
 
-os.environ['WANDB_CACHE_DIR'] = '/work3/s184399/logs/wandb'
-os.environ['WANDB_PROJECT'] = 'lima_ft'
+test = True    # Flag for whether we are testing the code or not. If true, we pass a single batch and overfit to that...
 
+os.environ['WANDB_PROJECT'] = 'lima_ft'
+os.environ['WANDB_DIR'] = '/work3/s184399/logs/wandb'
+os.environ['WANDB_CACHE_DIR'] = '/work3/s184399/cache_dir/wandb'
 
 model_name = "facebook/opt-125m"   # Small enough to do fairly fast generation on CPU (0.8s per small sentence) => Might be good for development, debugging and testing...
 dataset_name = "GAIR/lima"
@@ -30,15 +34,38 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# Load dataset
-ds = load_dataset(dataset_name, 'plain_text')
+# Dataset setup
+## Data collator: To tell the trainer how to split and mask the dataset (in teacher forcing)
+instruction_template = "### Human:"
+response_template = "### Assistant:"
+collator = DataCollatorForCompletionOnlyLM(instruction_template=instruction_template, response_template=response_template, tokenizer=tokenizer, mlm=False)
+    # DataCollatorForCompletionOnlyLM finds the instruction template (by looping through the tokens - as we could easily implement ourselves) and cuts. Then it cuts again after the assistant template.
+    # The cuts are, however, not made in the input_ids... Here the model is being given the answers too.... But the labels are set to -100 for everything until the answer begins, making the loss function ignore them...
+    # Hence it implements teacher forcing when trained....
+    # The trainer's dataloader (evidently) calls its DataCollatorForCompletionOnlyLM.torch_call method either during dataset building or during fetching
 
 def process_ds(ds):
-    return ds.map(lambda x: {"prompt": x["conversations"][0], "completion": x["conversations"][1]}, remove_columns=["conversations", "source"])
+    def aux(example):
+        x = example["conversations"]
+        assert len(x) == 2
+        #return {"prompt": x[0], "completion": x[1]}
+        return {'text': f"{instruction_template} {x[0]}{response_template} {x[1]}"}
+    
+    return ds.filter(
+        lambda x: x["source"] != "multi_turn"    # We only want the instruction tuning bit
+    ).map(
+        aux, remove_columns=["conversations", "source"]
+    )
 
-train_ds = process_ds(ds["train"])
+ds = load_dataset(dataset_name, 'plain_text')
+train_ds = process_ds(ds["train"]).train_test_split(train_size=1 if test else 0.8)   # Test size can be given as fraction or absolute number.... Also useful for testing.
+train_split, val_split = train_ds["train"], train_ds["test"]
 eval_ds = ds["test"]["conversations"]
 len_eval = len(eval_ds)
+len_train_split = len(train_split)
+
+#code_test_ds = Dataset.from_dict(train_ds["train"][0:1])  # Single batch
+#if test: train_split = code_test_ds
 
 # Define training args
 class ExampleCallback(WandbCallback):
@@ -46,35 +73,47 @@ class ExampleCallback(WandbCallback):
         model = kwargs["model"]
         tokenizer = kwargs["tokenizer"]
 
-        prompt = eval_ds[np.random.randint(len_eval)][0]
+        #prompt = eval_ds[np.random.randint(len_eval)][0]
+        example = train_split[np.random.randint(len_train_split)]
+        text = example["text"]
+        prompt, suggested_completion = text.split(response_template)
+        prompt = f"{prompt}{response_template} "
         tokenized_prompt = tokenizer.encode(prompt, return_tensors="pt")  # Why is this not a dictionary???
-        tokenized_completion = model.generate(tokenized_prompt.cuda(), max_new_tokens=100)
+        tokenized_completion = model.generate(tokenized_prompt.cuda(), max_new_tokens=1024)
         completion = tokenizer.decode(token_ids=tokenized_completion.squeeze(0), skip_special_tokens=True)
+        raw_completion = completion
+        
+        # Formatting to look nice
+        completion = completion.split(response_template)[1]
+        prompt = prompt.replace(instruction_template, '').replace(response_template, '')
 
         # Log to wandb
-        text_table = wandb.Table(columns=["prompt", "completion"])
-        text_table.add_data(prompt, completion)
+        text_table = wandb.Table(columns=["prompt", "suggested completion", "completion", "raw_completion"])
+        text_table.add_data(prompt, suggested_completion, completion, raw_completion)
         wandb.log({"examples": text_table})
 
 training_args = TrainingArguments(
-    num_train_epochs=3,
-    per_device_train_batch_size=16,
+    num_train_epochs=200 if test else 3,
+    per_device_train_batch_size=1 if test else 16,
     lr_scheduler_type="cosine",
     gradient_accumulation_steps=1,
     gradient_checkpointing=True,
-    logging_steps=1,
+    logging_steps=25,
     do_train=True,
     output_dir="/work3/s184399/checkpoints/lima-opt-125m",
     logging_dir="/work3/s184399/logs/lima-opt-125m",
     overwrite_output_dir=True,
     report_to="wandb",
-    run_name="finetune-opt-125m",
+    run_name="opt-125m code test" if test else "finetune-opt-125m",
 )
 trainer = SFTTrainer(
     model=model,
     tokenizer=tokenizer,
     args=training_args,
-    train_dataset=train_ds,
+    data_collator=collator,
+    dataset_text_field="text",
+    train_dataset=train_split, #train_ds,
+    eval_dataset=val_split,
     max_seq_length=1024,
     callbacks=[ExampleCallback],
 )
