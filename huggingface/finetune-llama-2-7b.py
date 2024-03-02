348b40eaae5d7879f6c88bdebfc6342c62155c41
MikkelGodsk <mikkelgodskj@hotmail.com>
2024-03-02 14:29:44 +0100

    Llama 2 training script (not tested) + opt-125m training fix...

--- huggingface/finetune-llama-2-7b.py
+++ huggingface/finetune-llama-2-7b.py
@@ -21,17 +21,16 @@ TODO:  Reproduce LIMA experiment &
 - PEFT
 """
 
-test = False    # Flag for whether we are testing the code or not. If true, we pass a single batch and overfit to that...
+test = True    # Flag for whether we are testing the code or not. If true, we pass a single batch and overfit to that...
 
-os.environ['WANDB_PROJECT'] = 'lima_ft'
+os.environ['WANDB_PROJECT'] = 'lima_ft_llama-2'
 os.environ['WANDB_DIR'] = '/work3/s184399/logs/wandb'
 os.environ['WANDB_CACHE_DIR'] = '/work3/s184399/cache_dir/wandb'
 
-model_name = "facebook/opt-125m"   # Small enough to do fairly fast generation on CPU (0.8s per small sentence) => Might be good for development, debugging and testing...
+model_name = "meta-llama/Llama-2-7b"
 dataset_name = "GAIR/lima"
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")  # Not sure if the device map enables FSDP by default...
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # Dataset setup
@@ -49,7 +48,7 @@ def process_ds(ds):
         x = example["conversations"]
         assert len(x) == 2
         #return {"prompt": x[0], "completion": x[1]}
-        return {'text': f"{instruction_template}\n{x[0]}\n\n{response_template}\n{x[1]}"}   # This article might have an opinion on this: https://www.philschmid.de/instruction-tune-llama-2
+        return {'text': f"{instruction_template} {x[0]}{response_template} {x[1]}"}
     
     return ds.filter(
         lambda x: x["source"] != "multi_turn"    # We only want the instruction tuning bit
@@ -58,27 +57,21 @@ def process_ds(ds):
     )
 
 ds = load_dataset(dataset_name, 'plain_text')
-train_ds = process_ds(ds["train"]).train_test_split(train_size=1 if test else 0.8)   # Test size can be given as fraction or absolute number.... Also useful for testing.
-train_split, val_split = train_ds["train"], train_ds["test"]
+train_ds = process_ds(ds["train"])
+if test: train_ds = Dataset.from_dict(train_ds["train"][0:1])  # Single batch for debugging purposes
 eval_ds = ds["test"]["conversations"]
 len_eval = len(eval_ds)
-len_train_split = len(train_split)
+len_train_ds = len(train_ds)
 
-#code_test_ds = Dataset.from_dict(train_ds["train"][0:1])  # Single batch
-#if test: train_split = code_test_ds
 
 # Define training args
 class ExampleCallback(WandbCallback):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.text_table = wandb.Table(columns=["prompt", "suggested completion", "completion", "raw_completion"])
-
     def on_log(self, args, state, control, logs=None, **kwargs):
         model = kwargs["model"]
         tokenizer = kwargs["tokenizer"]
 
-        #prompt = eval_ds[np.random.randint(len_eval)][0]
-        example = train_split[np.random.randint(len_train_split)]
+        # Sample training examples (mostly for debugging purposes...)
+        example = train_ds[np.random.randint(len_train_ds)]
         text = example["text"]
         prompt, suggested_completion = text.split(response_template)
         prompt = f"{prompt}{response_template} "
@@ -92,22 +85,33 @@ class ExampleCallback(WandbCallback):
         prompt = prompt.replace(instruction_template, '').replace(response_template, '')
 
         # Log to wandb
-        self.text_table.add_data(prompt, suggested_completion, completion, raw_completion)
-        wandb.log({"examples": self.text_table})
+        text_table = wandb.Table(columns=["prompt", "suggested completion", "completion", "raw_completion"])
+        text_table.add_data(prompt, suggested_completion, completion, raw_completion)
+        wandb.log({"Training examples": text_table})
+        
+        # Evaluate on eval set
+        eval_table = wandb.Table(columns=["prompt", "completion"])
+        for prompt in eval_ds:
+            prompt = prompt[0]
+            tokenized_prompt = tokenizer.encode(prompt, return_tensors="pt")
+            tokenized_completion = model.generate(tokenized_prompt.cuda(), max_new_tokens=1024)
+            completion = tokenizer.decode(token_ids=tokenized_completion.squeeze(0), skip_special_tokens=True)
+            eval_table.add_date(prompt, completion)
+        wandb.log({"eval": eval_table})
 
 training_args = TrainingArguments(
-    num_train_epochs=200 if test else 15,  # 15 epochs in the paper...
+    num_train_epochs=200 if test else 3,
     per_device_train_batch_size=1 if test else 16,
     lr_scheduler_type="cosine",
     gradient_accumulation_steps=1,
     gradient_checkpointing=True,
     logging_steps=25,
     do_train=True,
-    output_dir="/work3/s184399/checkpoints/lima-opt-125m",
-    logging_dir="/work3/s184399/logs/lima-opt-125m",
+    output_dir="/work3/s184399/checkpoints/lima-llama-2-7b",
+    logging_dir="/work3/s184399/logs/lima-llama-2-7b",
     overwrite_output_dir=True,
     report_to="wandb",
-    run_name="opt-125m code test" if test else "finetune-opt-125m",
+    run_name="llama-2-7b code test" if test else "finetune-llama-2-7b",
 )
 trainer = SFTTrainer(
     model=model,
@@ -115,9 +119,8 @@ trainer = SFTTrainer(
     args=training_args,
     data_collator=collator,
     dataset_text_field="text",
-    train_dataset=train_split, #train_ds,
-    eval_dataset=val_split,
-    max_seq_length=2048,   # It seems like the UserWarning with not being able to find the instruction and response templates were fixed when doubling max_seq_length and changing the template slightly....
+    train_dataset=train_ds,
+    max_seq_length=1024,
     callbacks=[ExampleCallback],
 )
 trainer.train()
---
+++ huggingface/finetune-llama-2-7b.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+### GPU and CPU related parameters for a simple GPU job script
+### select a GPU queue
+#BSUB -q gpua100
+### request the number of GPUs
+#BSUB -gpu "num=2:mode=exclusive_process"
+#BSUB -R "select[gpu80gb]"
+### request the number of CPU cores (at least 4x the number of GPUs)
+#BSUB -n 8
+### we want to have this on a single node
+#BSUB -R "span[hosts=1]"
+### we need to request CPU memory, too (note: this is per CPU core)
+#BSUB -R "rusage[mem=10GB]"
+
+### -- set the job Name --
+#BSUB -J Finetune_llama-2-7b-lima
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 24:00
+
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u s184399@student.dtu.dk
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o gpu_%J.out
+#BSUB -e gpu_%J.err
+
+source $HOME/miniconda3/bin/activate
+
+module load cuda/12.1
+module load cudnn/v8.9.1.23-prod-cuda-12.X
+conda activate msc
+export CUDA_LAUNCH_BLOCKING=1
+
+#export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+cd $HOME/msc/huggingface
+python finetune-llama-2-7b.py
