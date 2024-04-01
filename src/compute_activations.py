921079497327e038f8d3924b48d1819e03758d5c
MikkelGodsk <96190175+MikkelGodsk@users.noreply.github.com>
2024-04-01 13:08:22 +0200

    First bit of the concept guidance implementation

---
+++ src/compute_activations.py
@@ -0,0 +1,61 @@
+import torch
+import os
+import pandas as pd
+import numpy as np
+from tqdm.notebook import tqdm   # Change this
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from typing import List
+
+dataset_dir = 'geometry-of-truth/datasets'
+dtype = np.float32
+model_name = 'meta-llama/Llama-2-7b-hf'
+
+
+class Hook:
+  # Inspired by https://github.com/saprmarks/geometry-of-truth/blob/main/generate_acts.py
+  def __init__(self):
+    self.activations = []
+    self.idx = -1   # The index of the token we look at the internal state for
+
+  def __call__(self, module, args, output, **kwargs):
+    o = output[0][..., self.idx,:].detach().cpu().numpy().astype(dtype)
+    self.activations.append(o)
+
+
+def compute_activations(statements: List[str], model: torch.nn.Module, tokenizer) -> np.ndarray:
+  """
+    Returns:
+    - Activations of shape [num_layers, num_samples, n_hidden_dim]
+  """
+  hooks = []
+  handles = []
+  for i, layer in enumerate(model.model.layers):
+    hook = Hook()
+    handle = layer.register_forward_hook(hook)
+    hooks.append(hook)
+    handles.append(handle)
+
+  for statement in tqdm(statements):
+    tokens = tokenizer.encode(statement, return_tensors='pt').cuda()
+    _ = model(tokens)
+
+  for handle in handles:
+    handle.remove()
+
+  activations = []
+  for hook in hooks:
+    activations.append(np.vstack(hook.activations))
+
+  return np.stack(activations, axis=0)
+
+
+
+#factory = Factory.spawn_factory('llama2', version='7b')
+#model = factory.spawn_model()
+#tokenizer = factory.spawn_tokenizer()
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+
+common_claim_df = pd.read_csv(os.path.join(dataset_dir, 'common_claim_true_false.csv'))
+activations = compute_activations(common_claim_df['statement'][0:5], model, tokenizer)
+np.savez('common_claim_true_false.npz', activations=activations, labels=common_claim_df['label'])
